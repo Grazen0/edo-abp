426ba2600e6b3b75f754e0faaa78570944360c63
Grazen0 <josedanielgrayson@proton.me>
2025-02-18 12:27:18 -0500

    okay

--- rk4.py
+++ rk4.py
@@ -4,7 +4,7 @@ import numpy as np
 
 def rk4_system(t0, x0, h, steps, f):
     t = np.linspace(t0, t0 + h * steps, steps + 1)
-    x = np.empty((len(x0), steps + 1))
+    x = np.empty((len(x0), steps + 1), dtype=np.float128)
     x[:, 0] = x0
 
     for i in range(steps):
@@ -24,9 +24,9 @@ def make_damping_matrix(k):
     n = len(k)
 
     if n == 1:
-        return np.array([k[0]])
+        return np.array([[k[0]]], dtype=np.float128)
 
-    out = np.zeros((n, n))
+    out = np.zeros((n, n), dtype=np.float128)
     out[0][0] = k[0] + k[1]
     out[0][1] = -k[1]
     out[n - 1][n - 2] = -k[n - 1]
@@ -40,35 +40,44 @@ def make_damping_matrix(k):
     return out
 
 
-m_mat = np.diag([500, 450, 425])
-c_mat = make_damping_matrix([300, 100, 80])
-k_mat = make_damping_matrix([20, 20, 20])
+mass = np.array([1.5e5, 1.5e5, 1.5e5], dtype=np.float128)
+elasticity = np.array([2e5, 2e5, 2e5], dtype=np.float128)
+damping_percent = np.array([0.05, 0, 0], dtype=np.float128)
+n = len(mass)
 
-n = len(m_mat)
+freq = 4
+ground_accel = 0.3
 
 t0 = 0
-x0 = np.zeros(2 * n)
-tN = 50
-steps = 400
-h = (tN - t0) / steps
+x0 = np.zeros(2 * n, dtype=np.float128)
+ts = 60
+s = 10_000
 
-m_inv = np.linalg.inv(m_mat)
+h = (ts - t0) / s
+
+damping = damping_percent * 2 * np.sqrt(mass * elasticity)
+c_mat = make_damping_matrix(damping)
+k_mat = make_damping_matrix(elasticity)
+
+m_inv = np.diag(1 / mass)
 
 
 def p(t):
-    out = np.zeros(n)
-    out[0] = 10 * np.cos(t)
+    out = np.zeros(n, dtype=np.float128)
+    out[0] = mass[0] * ground_accel * np.sin(2 * np.pi * freq * t)
     return out
 
 
 def f(t, x):
     u = x[:n]
     v = x[n:]
-    v_prime = m_inv @ (-c_mat @ v - k_mat @ u + p(t))
-    return np.append(u_prime, v_prime)
+    return np.append(
+        v,
+        m_inv @ (-c_mat @ v - k_mat @ u + p(t)),
+    )
 
 
-(t_out, x_out) = rk4_system(t0, x0, h, steps, f)
+(t_out, x_out) = rk4_system(t0, x0, h, s, f)
 u_out = x_out[:n]
 
 colors = plt.colormaps["gist_rainbow"](np.linspace(0, 1, n))
